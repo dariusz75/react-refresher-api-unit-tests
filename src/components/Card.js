992376c73d68d287d6addb78fb3039dcf86b36f7
Dariusz Franczak <dariuszfranczak@Dariuszs-MacBook-Pro.local>
2023-03-16 23:21:56 +0000

    movies rendered

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,25 @@
+import { Link } from "react-router-dom";
 
-export const Card = () => {
+import backup from '../assets/images/backup.png';
+
+export const Card = ({movie}) => {
+ const {id, original_title, overview, poster_path } = movie;
+
+  const cardContainer = 'max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 m-3';
+  const cardTitle = 'mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white';
+  const cardText = 'mb-3 font-normal text-gray-700 dark:text-gray-400';
+
   return (
-    <div>Card</div>
+    <div className={cardContainer}>
+      <Link to={`/movie/${id}`}>
+        <img className="rounded-t-lg" src={poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : backup} alt="movie poster" />
+      </Link>
+      <div className="p-5">
+        <Link to={`/movie/${id}`}>
+          <h5 className={cardTitle}>{original_title}</h5>
+        </Link>
+        <p className={cardText}>{overview}</p>
+      </div>
+    </div>
   )
-}
+};
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react";
+import { Card } from "../components";
 
 export const MovieList = () => {
+  const [movies, setMovies] = useState([1, 2]);
+
+  useEffect(() => {
+    async function fetchMovies() {
+      const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=f2960d24d3589db02d7917c654ff10fe');
+      const data = await response.json();
+      console.log('movies are', data)
+      setMovies(data.results);
+    };
+    fetchMovies();
+  },[]);
+
   return (
-    <main>MovieList</main>
+    <main>
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-wrap">
+          {movies.map((movie) => {return <Card key={movie.id} movie={movie} />}
+          )}
+        </div>
+      </section>
+    </main>
   )
 }
