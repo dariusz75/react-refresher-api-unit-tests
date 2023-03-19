8d431d0b631acb505b58be720e2f838f828e2da5
Dariusz Franczak <dariuszfranczak@Dariuszs-MacBook-Pro.local>
2023-03-19 23:15:12 +0000

    search functionality implemented

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
 
-export const useFetch = (apiPath) => {
+export const useFetch = (apiPath, queryTerm='') => {
   const basePath = process.env.REACT_APP_BASE_PATH;
   const apiKey = process.env.REACT_APP_API_KEY;
   const [data, setData] = useState([]);
-  const url = `${basePath}/${apiPath}?api_key=${apiKey}`;
+  const url = `${basePath}/${apiPath}?api_key=${apiKey}&query=${queryTerm}`;
 
   useEffect(() => {
     async function fetchMovies() {
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,6 +1,33 @@
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 
-export const Search = () => {
+import { useFetch } from "../hooks/useFetch";
+
+import { Card } from "../components";
+
+export const Search = ({apiPath}) => {
+  const [ searchParams ] = useSearchParams();
+  const queryTerm = searchParams.get('q');
+
+  const {data: movies} = useFetch(apiPath, queryTerm);
+
+  useEffect(() => {
+    console.log('queryTerm', queryTerm);
+  })
+
   return (
-    <main>Search</main>
+    <main>
+      <section className="py-7">
+        <p className="text-3xl text-grey-700 dark:text-white">
+          {movies.length === 0 ? `No fesult found for ${queryTerm}` : `${movies.length} results found for ${queryTerm}`}
+        </p>
+      </section>
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-wrap">
+          {movies.map((movie) => {return <Card key={movie.id} movie={movie} />}
+          )}
+        </div>
+      </section>
+    </main>
   )
 }
